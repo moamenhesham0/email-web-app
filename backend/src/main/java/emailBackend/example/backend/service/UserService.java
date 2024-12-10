1cd8f8afe1b25ff3b893e5fedbe16a9a09ca8528
muhannd2004 <muhanndsayed24@gmail.com>
2024-12-10 02:23:04 +0200

    don't touch my work i will continue on it
    i should handle lots of thing so its confusing if you see a bug just text me don't touch my code

--- backend/src/main/java/emailBackend/example/backend/service/UserService.java
+++ backend/src/main/java/emailBackend/example/backend/service/UserService.java
@@ -1,23 +1,33 @@
 package emailBackend.example.backend.service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import emailBackend.example.backend.classes.Email;
-import emailBackend.example.backend.repository.EmailRepository;
+import emailBackend.example.backend.repository.UserRepository;
 
 @Service
-public class EmailService {
+public class UserService {
     @Autowired
-    private EmailRepository emailRepository;
+    private UserRepository emailRepository;
 
     public void createEmail(Email email) {
+        email.setId(UUID.randomUUID().toString()); 
+        email.setTimeStamp(LocalDateTime.now());
+
         emailRepository.save(email);
     }
 
        public Optional<Email> getEmailById(String id) {
         return emailRepository.findById(id);
     }
+
+    public void deleteEmailById(String id) {
+       emailRepository.deleteEmailById(id);
+    }
+    
 }
