88268a054359b8bc9419686006375e59db60ef7b
Moamen <moamen.hesham.code@gmail.com>
2024-12-11 13:01:29 +0200

    pushed

--- backend/src/main/java/emailBackend/example/backend/classes/Email.java
+++ backend/src/main/java/emailBackend/example/backend/classes/Email.java
@@ -3,59 +3,99 @@ package emailBackend.example.backend.classes;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
 
 public class Email implements Serializable{
+    private static HashMap<String , Integer>  prioritiesToInt = new Hashmap<>();
+    private static HashMap<Integer , String>  prioritiesToString = new Hashmap<>();
+    static{
+    prioritiesToInt.put("Urgent" , 4);
+    prioritiesToInt.put("Important" , 3);
+    prioritiesToInt.put("Moderate" , 2);
+    prioritiesToInt.put("Minor" , 1);
+    for (Map.Entry<String, Integer> en : prioritiesToInt.entrySet()) {
+        String key = en.getKey();
+        Integer val = en.getValue();
+        prioritiesToString.put(val , key);
+    }
+    }
+    private String sender;
     private String recipient;
     private String subject;
     private String textBody;
     private LocalDateTime timeStamp;
     private boolean read = false;
     private List<Attachment> attachments;
+    private int priority; 
     private String id ;
     
+    public String getSender() {
+        return this.sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
     public String getRecipient() {
-        return recipient;
+        return this.recipient;
     }
     public void setRecipient(String recipient) {
         this.recipient = recipient;
     }
+
     public String getSubject() {
-        return subject;
+        return this.subject;
     }
     public void setSubject(String subject) {
         this.subject = subject;
     }
+
     public String getTextBody() {
-        return textBody;
+        return this.textBody;
     }
     public String getId() {
-        return id;
+        return this.id;
     }
     
     public void setTextBody(String textBody) {
         this.textBody = textBody;
     }
     public LocalDateTime getTimeStamp() {
-        return timeStamp;
+        return this.timeStamp;
     }
+
     public void setTimeStamp(LocalDateTime timeStamp) {
         this.timeStamp = timeStamp;
     }
+
     public boolean isRead() {
-        return read;
+        return this.read;
     }
     public void setRead(boolean read) {
         this.read = read;
     }
+
     public List<Attachment> getAttachments() {
-        return attachments;
+        return this.attachments;
     }
     public void setAttachments(List<Attachment> attachments) {
         this.attachments = attachments;
     }
+
     public void setId(String id) {
         this.id = id;
+    }
+    public void setPriority(String priority)
+    {
+        this.priority = prioritiesToInt.get(priority);
+    }
+    public int getPriorityInt()
+    {
+        return this.priority;
+    }
+    public String getPriorityString()
+    {
+        return prioritiesToString.get(this.priority);
     }
     
     
