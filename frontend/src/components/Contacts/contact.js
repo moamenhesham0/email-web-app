d1eb58c5255cf74ac94c82898d5180c1982c1ccc
AmoAl2ntel <154479514+AhmedAliElashmawy@users.noreply.github.com>
2024-12-19 08:08:26 +0200

    Update contact.js

--- frontend/src/components/Contacts/contact.js
+++ frontend/src/components/Contacts/contact.js
@@ -6,6 +6,8 @@ import LabelImportantOutlinedIcon from "@mui/icons-material/LabelImportantOutlin
 import { useNavigate } from 'react-router-dom';
 import { selectMail } from "../../features/mailSlice";
 import { useDispatch } from "react-redux";
+import EditIcon from "@mui/icons-material/Edit";
+
 
 function Contact({userName,emailAddress }) {
 
@@ -30,6 +32,9 @@ function Contact({userName,emailAddress }) {
   return (
     <div onClick={openMail} className="emailRow">
       <Checkbox></Checkbox>
+      <IconButton>
+        <EditIcon fontSize="small" />
+      </IconButton>
       <h3 className="emailRow-title">{userName}</h3>
     </div>
   );
